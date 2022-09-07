1529089ce8bb43025cc661d204231182e50d5f85
xbernardoalvez66 <55824722+xbernardoalvez66@users.noreply.github.com>
2022-09-07 19:47:14 -0300

    Add files via upload

---
+++ blog-simples-y-receptivo-html/script.js
@@ -0,0 +1,14 @@
+const menuToggle = document.querySelector('.menuToggle');
+const navigation = document.querySelector('.navigation');
+
+menuToggle.addEventListenner('click',function(){
+	menuToggle.classList.toggle('active');
+	navigation.classList.toggle('active');
+
+	if (menuToggle.classList.contains('active')){
+		menuToggle.innerHTML = '<i class="fas fa-times"></i>';
+	} else{
+		menuToggle.innerHTML = '<i class="fas fa-bars"></i>';
+	}
+
+});
